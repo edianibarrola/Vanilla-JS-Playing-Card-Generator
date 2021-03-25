886d96c14b738814fc0e578e19afe0236e356b15
edianibarrola <edianibarrola@gmail.com>
2021-03-25 21:50:30 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let cardNumber = document.querySelector(".card h1");
+  let suit = ["heart", "diamond", "club", "spade"];
+
+  let numbers = ["A", "1", "2", "3", "4"];
+
+  card.classList.add(suit[getRandomInt(0, suit.length - 1)]);
+  cardNumber.innerHTML = numbers[getRandomInt(0, numbers.length - 1)];
 };
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
